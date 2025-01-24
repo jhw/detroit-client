a378bd84a6e8e20845c8f7ade7ce38b99a1c0b0a
Justin Worrall <justin.worrall@gmail.com>
2025-01-24 13:13:08 +0000

    updated setenv

--- setenv.sh
+++ setenv.sh
@@ -3,7 +3,7 @@
 export AWS_PROFILE=woldeploy
 export AWS_REGION=eu-west-1
 export PYTHONPATH=.
-export DETROIT_SAMPLES_STACK_NAME=detroit-factory
-export DETROIT_SAMPLES_API_ENDPOINT=https://detroit-factory.klingklangwol.com
+export DETROIT_SAMPLES_STACK_NAME={stack_name_here}
+export DETROIT_SAMPLES_API_ENDPOINT={api_endpoint_here}
 export DETROIT_SAMPLES_CUTOFF=250
 export DETROIT_SAMPLES_BATCH_SIZE=4
